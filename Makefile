f3e57717ccb524a71a02cb051c2882b9a322e4a9
Addam Dominec <a.dominec@gmail.com>
2013-06-29 17:51:42 +0200

    Poisson reconstruction: try to filter the output of lowres data

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CXXFLAGS = -g
 
 OPENCV_LIBS = -lopencv_core -lopencv_calib3d -lopencv_video -lopencv_highgui -lopencv_imgproc -lopencv_flann
 ALPHA_SHAPES_LIBS = -lCGAL -lboost_thread -lgmp
-PCL_LIBS = -lpcl_common -lpcl_kdtree -lpcl_search -lpcl_surface
+PCL_LIBS = -lpcl_common -lpcl_kdtree -lpcl_search -lpcl_surface -lpcl_features
 RENDER_glx_LIBS = -lGL -lGLEW -lopencv_highgui -lX11
 
 LIBS = ${ALPHA_SHAPES_LIBS} ${RENDER_${SYSTEM_OPENGL}_LIBS} ${OPENCV_LIBS} ${PCL_LIBS}
--- pcl.cpp
+++ pcl.cpp
@@ -17,10 +17,16 @@
 #else
 	#include "recon.hpp"
 #endif
+
+#define P2(x) (x)*(x)
+
 typedef pcl::PointCloud<pcl::PointNormal> NormalCloud;
 
-NormalCloud::Ptr convert(const Mat points, const Mat normals)
+Mat estimatedNormals(Mat points);
+
+NormalCloud::Ptr convert(const Mat points, const Mat inormals)
 {
+	Mat normals = estimatedNormals(points);
 	NormalCloud::Ptr cloud(new NormalCloud);
 	assert(points.rows == normals.rows);
 	cloud->reserve(points.rows);
@@ -91,12 +97,84 @@ int convert(Mesh dst, const pcl::PolygonMesh &mesh)
 	return 0;
 }
 
-Mesh poissonSurface(const Mat points, const Mat normals)
+// filter out all faces larger than given size, and their vertices
+void filterFinest(Mesh &mesh, float size)
+{
+	#ifdef TEST_BUILD
+	printf("Before filtering: %i vertices, %i faces. Filtering size %g.\n", mesh.vertices.rows, mesh.faces.rows, size);
+	#endif
+	// mark accepted faces and vertices touched by any of these
+	vector<bool> goodFaces(mesh.faces.rows, true), goodVertices(mesh.vertices.rows, false);
+	for (int i=0; i<mesh.faces.rows; i++) {
+		bool goodFace = true;
+		int32_t* verts = mesh.faces.ptr<int32_t>(i);
+		for (char j=0; j<3; j++) {
+			float aw = mesh.vertices.at<float>(verts[j],3), bw = mesh.vertices.at<float>(verts[(j+1)%3],3);
+			Mat d = mesh.vertices.row(verts[j])/aw - mesh.vertices.row(verts[(j+1)%3])/bw;
+			float distance = cv::norm(d);
+			if (distance > size) {
+				goodFace = false;
+				break;
+			}
+		}
+		if (goodFace) {
+			for (char j=0; j<3; j++) {
+				goodVertices[verts[j]] = true;
+			}
+		}
+		goodFaces[i] = goodFace;
+	}
+	// filter out the mesh structure
+	vector<int32_t> reindex(mesh.vertices.rows, -1);
+	int32_t writeIndex = 0;
+	for (int i=0; i<mesh.vertices.rows; i++) {
+		if (goodVertices[i]) {
+			if (writeIndex < i)
+				mesh.vertices.row(i).copyTo(mesh.vertices.row(writeIndex));
+			reindex[i] = writeIndex;
+			writeIndex += 1;
+		}
+	}
+	mesh.vertices.resize(writeIndex);
+	writeIndex = 0;
+	for (int i=0; i<mesh.faces.rows; i++) {
+		if (goodFaces[i]) {
+			int32_t *dst = mesh.faces.ptr<int32_t>(writeIndex ++),
+			        *src = mesh.faces.ptr<int32_t>(i);
+			for (char j=0; j<3; j++) {
+				assert (goodVertices[src[j]]);
+				assert (reindex[src[j]] >= 0);
+				dst[j] = reindex[src[j]];
+			}
+		}
+	}
+	mesh.faces.resize(writeIndex);
+	#ifdef TEST_BUILD
+	printf("After filtering: %i vertices, %i faces. Filtering size %g.\n", mesh.vertices.rows, mesh.faces.rows, size);
+	#endif
+}
+
+// returns largest dimension of the bounding box
+float boundingBoxSize(const Mat points)
+// expects Cartesian points in rows, ignores 4th column if present
+{
+	float size = 0;
+	for (char j=0; j<3; j++) {
+		double min, max;
+		minMaxIdx(points.col(j), &min, &max);
+		if (max-min > size)
+			size = max - min;
+	}
+	return size;
+}
+
+Mesh poissonSurface(const Mat points, const Mat normals, int degree)
 {
 	NormalCloud::Ptr cloud(convert(points, normals));
 	
 	pcl::Poisson<pcl::PointNormal> poisson;
-	poisson.setDegree(3);
+	poisson.setDegree(4);
+	poisson.setIsoDivide(degree);
 	poisson.setOutputPolygons(false);
 	poisson.setInputCloud (cloud);
 	
@@ -106,7 +184,18 @@ Mesh poissonSurface(const Mat points, const Mat normals)
 	Mesh result(Mat(mesh.cloud.width * mesh.cloud.height, 4, CV_32FC1), Mat(mesh.polygons.size(), 3, CV_32SC1));
 	//pcl::io::saveOBJFile("temp_poisson.obj", mesh);
 	convert(result, mesh);
+	
+	float bbox = boundingBoxSize(result.vertices);
+	float gridSize = bbox / (1<<(poisson.getDepth()));
+	printf("boundbox %g, isodivide %i, depth %i, scale %g, gridsize %g\n", bbox, poisson.getIsoDivide(), poisson.getDepth(), poisson.getScale(), gridSize);
+	filterFinest(result, 1.8*gridSize);
+	
 	return result;
+}
+
+Mesh poissonSurface(const Mat points, const Mat normals)
+{
+	return poissonSurface(points, normals, 4);
 }
 
 Mesh rbfSurface(const Mat points, const Mat normals)
@@ -159,7 +248,6 @@ Mesh greedyProjection(const Mat points, const Mat normals)
 	return result;
 }
 
-#ifdef TEST_BUILD
 Mat estimatedNormals(Mat points)
 {
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
@@ -192,12 +280,13 @@ Mat estimatedNormals(Mat points)
 	}
   return result;
 }
-int main()
+#ifdef TEST_BUILD
+int main(int argc, char**argv)
 {
 	//read input
 	std::ifstream is("shit/bunny_1000");
 	//std::ifstream is("shit/stanford_dragon_big");
-	std::ofstream os("shit/bunny_greedyprojection.obj");
+	std::ofstream os("shit/bunny_poisson.obj");
   os.precision (5);
 	int n;
 	is >> n;
@@ -219,8 +308,12 @@ int main()
 	}
 	std::cout << "Calculating normals..." << std::endl;
 	Mat normals = estimatedNormals(points);
-	std::cout << "Calculating surface..." << std::endl;
-	Mesh result = poissonSurface(points, normals);//greedyProjection(points, normals);
+	int degree = 4;
+	if (argc > 1)
+		degree = atoi(argv[1]);
+	std::cout << "Calculating surface at degree "<<degree <<"..." << std::endl;
+
+	Mesh result = poissonSurface(points, normals, degree);//greedyProjection(points, normals);
 	std::cout << result.vertices.rows << " vertices, " << result.faces.rows << " faces" << std::endl;
 	for(int i=0; i < result.vertices.rows; i ++) {
 		const float* row = result.vertices.ptr<float>(i);
@@ -231,6 +324,7 @@ int main()
 		os << "f " << row[0]+1 << ' ' << row[1]+1 << ' ' << row[2]+1 << std::endl;
 	}
 	os.close();
+
 	return EXIT_SUCCESS;
 }
 #endif
